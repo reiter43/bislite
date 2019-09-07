506744ad5b3d8895f4dd97f532534f980da7f3f3
reiter43 <43reiter@gmail.com>
2019-09-07 21:21:15 +0300

    Верстка страницы портфолио

--- src/js/script/script.js
+++ src/js/script/script.js
@@ -10,3 +10,37 @@ document.querySelector('.burger').addEventListener('click',  event => {
         document.querySelector('.nav').style.display = 'none'
     }	
 });
+
+
+// Фильтрация работ по категориям
+
+let buttonsPortf = document.querySelectorAll('.portfolio__button');
+
+buttonsPortf.forEach(function(btn){
+    btn.onclick = function(event){
+        event.preventDefault;
+
+        buttonsPortf.forEach(function(btn){
+            btn.classList.remove("portfolio__button--active");
+            event.target.classList.add("portfolio__button--active");            
+        })
+
+        let buttonFilter = btn.innerHTML;
+        console.log(buttonFilter);     
+
+        let portfolioItems = document.querySelectorAll('.worksItem--portfolio');
+        
+        portfolioItems.forEach(elem => {
+            elem.style.display = 'none';
+
+            if (buttonFilter == 'All'){
+                elem.style.display = 'block';
+            }
+
+            if(elem.classList.contains(buttonFilter)){            
+                elem.style.display = 'block';
+            }                
+        })
+    }
+})
+
